5f8674a12bfd42b1348bd23c60a02a7cb2900a31
Endy Muhardin <endy.muhardin@gmail.com>
2016-03-18 09:45:35 +0700

    query string

--- contoh-kode/belajar-http/src/main/java/com/muhardin/endy/belajar/http/HaloServlet.java
+++ contoh-kode/belajar-http/src/main/java/com/muhardin/endy/belajar/http/HaloServlet.java
@@ -13,15 +13,20 @@ import javax.servlet.annotation.WebServlet;
 public class HaloServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) 
 					throws IOException {
+
+		String nama = req.getParameter("nama");
+		String email = req.getParameter("email");
+
 		res.setContentType("text/html");
 		
 		PrintWriter output = res.getWriter();
 		output.println("<html>");
 		output.println("<head>");
-		output.println("<title>Halo Servlet</title>");
+		output.println("<title>Halo "+nama+"</title>");
 		output.println("</head>");
 		output.println("<body>");
-		output.println("<h1>Halo Servlet</h1>");
+		output.println("<h1>Halo "+nama+"</h1>");
+		output.println("Email : "+email);
 		output.println("</body>");
 		output.println("</html>");
 	}
